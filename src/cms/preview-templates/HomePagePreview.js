f23aa0ba125debc1a79c5437c34a2ffae47d3093
Hogan McDonald <hogan.developer@gmail.com>
2018-12-26 15:45:58 -0600

    creates preview cor use with cms

---
+++ src/cms/preview-templates/HomePagePreview.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { HomePageTemplate } from '../../pages/index';
+
+const AboutPagePreview = ({ entry, widgetFor }) => {
+  const home = {
+    seo: {
+      title: 'asdf',
+      description: 'asdf',
+      browserTitle: 'asdf'
+    }
+  };
+
+  debugger;
+
+  return <HomePageTemplate home={home} />;
+};
+
+AboutPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  widgetFor: PropTypes.func
+};
+
+export default AboutPagePreview;
