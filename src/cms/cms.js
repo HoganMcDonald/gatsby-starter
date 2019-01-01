3c553d88d1a58af0bd1eb9f8fd9f8299412ae7f6
Hogan McDonald <hogan.developer@gmail.com>
2018-12-31 19:57:21 -0600

    adds navbar preview template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,10 @@
 import CMS from 'netlify-cms';
 
 import HomePagePreview from './preview-templates/HomePagePreview';
+import NavBarPreview from './preview-templates/NavBarPreview';
 import typography from '../styles/typography';
 CMS.registerPreviewStyle(typography.toString(), { raw: true });
 
 // Previews
 CMS.registerPreviewTemplate('home', HomePagePreview);
+CMS.registerPreviewTemplate('navbar', NavBarPreview);
